fea00ce3e940a09529a72b2d248b8a014fc632b6
Andrew Joseph Moss <a.moss@sobolt.com>
2023-07-14 18:44:10 +0200

    part 1 implemented

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,17 @@ use nom::bytes::complete::tag;
 use nom::character::complete::one_of;
 use nom::multi::many1;
 use nom::sequence::separated_pair;
+use nom::branch::alt;
+use std::collections::HashMap;
 
 #[derive(PartialEq, Debug)]
-enum CMD<'a> {
+enum Line<'a> {
     CdUp,
     CdIn(&'a str),
     CdRoot,
-    Ls(Vec<File>),
+    Ls,
+    Dir(&'a str),
+    File(File)
 }
 
 #[derive(PartialEq, Debug)]
@@ -19,41 +23,69 @@ struct File {
 }
 
 pub fn process_part_1(input: &str) -> usize {
-    // input.lines().map(|line| {})
-    95437
+    let mut dirs = vec!["/"];
+    let mut dir_sizes = HashMap::<String, usize>::new();
+    input.lines().map(|line| parse_line(line).unwrap().1).for_each(|cmd| {
+        match cmd {
+            Line::CdUp => {
+                if dirs.len() > 1 {
+                    dirs.pop();
+                }
+            },
+            Line::CdRoot => {
+                dirs.truncate(1);
+            },
+            Line::CdIn(dir) => {
+                dirs.push(dir);
+            },
+            Line::Ls => (),
+            Line::Dir(_) => (),
+            Line::File(file) => {
+                let mut dir_path = String::from("");
+                dirs.iter().for_each(|dir| {
+                    dir_path += dir;
+                    dir_sizes.entry(dir_path.clone()).and_modify(|size| *size += file.size).or_insert(file.size);
+                });
+            }
+        }
+    });
+    
+    let sum_of_below_100_000: usize = dir_sizes.iter().filter(|(_, size)| **size <= 100_000).map(|(_, size)| size).sum();
+    sum_of_below_100_000
 }
 
-fn map_line_to_cmd(line: &str) -> CMD {
-    todo!();
+fn parse_line(input: &str) -> IResult<&str, Line> {
+    let (input, line) = alt((parse_cd, parse_dir, parse_file, parse_ls))(input)?;
+    Ok((input, line))
 }
 
-fn parse_ls(input: &str) -> IResult<&str, CMD> {
+fn parse_ls(input: &str) -> IResult<&str, Line> {
     let (input, _) = tag("$ ")(input)?;
-    Ok((input, CMD::Ls(Vec::<File>::new())))
+    Ok((input, Line::Ls))
 }
 
-fn parse_cd(input: &str) -> IResult<&str, CMD> {
+fn parse_cd(input: &str) -> IResult<&str, Line> {
     let (input, _) = tag("$ cd ")(input)?;
     let cmd = match input {
-        "/" => CMD::CdRoot,
-        ".." => CMD::CdUp,
-        dir => CMD::CdIn(dir)
+        "/" => Line::CdRoot,
+        ".." => Line::CdUp,
+        dir => Line::CdIn(dir)
     };
     Ok((input, cmd))
 }
 
-fn parse_dir(input: &str) -> IResult<&str, &str> {
+fn parse_dir(input: &str) -> IResult<&str, Line> {
     let (input, _) = tag("dir ")(input)?;
-    Ok((input, input))
+    Ok((input, Line::Dir(input)))
 }
 
-fn parse_file(input: &str) -> IResult<&str, File> {
+fn parse_file(input: &str) -> IResult<&str, Line> {
     let (input, (size, name)) = separated_pair(nom::character::complete::u32
         , tag(" "), many1(one_of("abcdefghijklmnopqrstuvwxyz.")))(input)?;
-    Ok((input, File {
+    Ok((input, Line::File(File {
         size: size.to_usize(),
         name: name.into_iter().collect()
-    }))
+    })))
 }
 
 
@@ -71,10 +103,44 @@ mod tests {
     }
 
     #[test]
+    fn test_parse_line() {
+        let input = "dir a";
+        let result = parse_line(input).unwrap().1;
+        assert_eq!(result, Line::Dir("a"));
+
+        let input = "5555 h.ytk";
+        let result = parse_line(input).unwrap().1;
+        assert_eq!(result, Line::File(File {
+            name: String::from("h.ytk"),
+            size: 5555
+        }));
+
+        let input = "$ cd /";
+        let result = parse_line(input).unwrap().1;
+        assert_eq!(result, Line::CdRoot);
+
+        let input = "$ cd ..";
+        let result = parse_line(input).unwrap().1;
+        assert_eq!(result, Line::CdUp);
+
+        let input = "$ cd a";
+        let result = parse_line(input).unwrap().1;
+        assert_eq!(result, Line::CdIn("a"));
+
+        let input = "$ ls";
+        let result = parse_line(input).unwrap().1;
+        assert_eq!(result, Line::Ls);
+
+        let input = "garbagesldkjfls";
+        let result = parse_line(input);
+        assert!(result.is_err());
+    }
+
+    #[test]
     fn test_parse_dir() {
         let input = "dir a";
         let result = parse_dir(input).unwrap().1;
-        assert_eq!(result, "a");
+        assert_eq!(result, Line::Dir("a"));
 
         let input = "safdjkosljs";
         let result = parse_dir(input);
@@ -85,10 +151,10 @@ mod tests {
     fn test_parse_file() {
         let input = "239849 g.txt";
         let result = parse_file(input).unwrap().1;
-        assert_eq!(result, File {
+        assert_eq!(result, Line::File(File {
             size: 239849,
             name: String::from("g.txt")
-        });
+        }));
 
         let input = "sdfijopsjfd";
         let result = parse_file(input);
@@ -99,7 +165,7 @@ mod tests {
     fn test_parse_ls() {
         let input = "$ ls";
         let result = parse_ls(&input).unwrap().1;
-        assert_eq!(result, CMD::Ls(Vec::<File>::new()));
+        assert_eq!(result, Line::Ls);
 
         let input = "askfjslf";
         let result = parse_ls(input);
@@ -110,19 +176,19 @@ mod tests {
     fn test_parse_cd() {
         let input = "$ cd /";
         let result = parse_cd(input).unwrap().1;
-        assert_eq!(result, CMD::CdRoot);
+        assert_eq!(result, Line::CdRoot);
 
         let input = "$ cd ..";
         let result = parse_cd(input).unwrap().1;
-        assert_eq!(result, CMD::CdUp);
+        assert_eq!(result, Line::CdUp);
 
         let input = "$ cd a";
         let result = parse_cd(input).unwrap().1;
-        assert_eq!(result, CMD::CdIn("a"));
+        assert_eq!(result, Line::CdIn("a"));
         
         let input = "$ cd d";
         let result = parse_cd(input).unwrap().1;
-        assert_eq!(result, CMD::CdIn("d"));
+        assert_eq!(result, Line::CdIn("d"));
 
         let input = "asdjfqksf";
         let result = parse_cd(input);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+use aoc_7;
+
 fn main() {
     println!("Hello, world!");
+    part_1();
+}
+
+fn part_1() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let result = aoc_7::process_part_1(&input);
+    println!("{}", result);
 }
